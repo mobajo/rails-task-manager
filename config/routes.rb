47b6f7e70b95ba0db4f4d950708f23f8d30c06b4
Morten Jorgensen <morten00@gmail.com>
2017-11-07 13:23:10 +0100

    Task manager with rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    # CREATE
-  # get 'restaurants/new',      to: "restaurants#new", as: 'new_restaurant'
-  # post 'restaurants',         to: "restaurants#create"
-  # # READ
-  # get 'restaurants',          to: "restaurants#index"
-  # get 'restaurants/:id',      to: "restaurants#show"
-  # # UPDATE
-  # get 'restaurants/:id/edit', to: "restaurants#edit", as: 'edit_restaurant'
-  # patch 'restaurants/:id',    to: "restaurants#update"
-  # # DELETE
-  # delete 'restaurants/:id',   to: "restaurants#destroy"
-  resources :restaurants
+#    Prefix Verb   URI Pattern               Controller#Action
+#     tasks GET    /tasks(.:format)          tasks#index
+#           POST   /tasks(.:format)          tasks#create
+#  new_task GET    /tasks/new(.:format)      tasks#new
+# edit_task GET    /tasks/:id/edit(.:format) tasks#edit
+#      task GET    /tasks/:id(.:format)      tasks#show
+#           PATCH  /tasks/:id(.:format)      tasks#update
+#           PUT    /tasks/:id(.:format)      tasks#update
+#           DELETE /tasks/:id(.:format)      tasks#destroy
+  resources :tasks
 end
